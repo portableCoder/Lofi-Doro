cfbd59a8acc2d7e9cd20926f539ca74d4cc4629a
portableCoder <>
2022-03-12 14:29:51 +0530

    changed favicon paths

--- components/SEOHead.tsx
+++ components/SEOHead.tsx
@@ -9,10 +9,10 @@ export default function SEOHead() {
         <meta name="description" content="A pomodoro timer to study/relax to" />
         <meta name="og:image" content="https://raw.githubusercontent.com/portableCoder/Lofi-Doro/master/assets/logo.png" />
 
-        <link rel="shortcut icon" href="/favicon.ico" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="shortcut icon" href="/Lofi-Doro/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/Lofi-Doro/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/Lofi-Doro/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/Lofi-Doro/favicon-16x16.png" />
     </Head>
 
 }
